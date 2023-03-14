d8d27bf25e6666f7927a5d77364ff26794d7575e
Wonhee Kang <eggfreitag@gmail.com>
2023-03-14 13:09:08 +0900

    data가 없을 때 반환되는 에러 확인 및 수정

--- client/src/screens/Users/Users.tsx
+++ client/src/screens/Users/Users.tsx
@@ -16,18 +16,21 @@ const suspenseOption = {
 };
 
 const Users = () => {
-  const {
-    data: { data },
-  } = useQuery<{ data: User[] }, Error>(["users"], fetchUsers, {
+  const { data } = useQuery<{ data: User[] }, Error>(["users"], fetchUsers, {
     ...suspenseOption,
     // cacheTime: 10000, // 10초간 캐시를 사용, 10초가 지나면 재요청 (default: 5분)
     // staleTime: 10000, // 캐시된 데이터가 만료되기 전에 사용할 수 있는 시간, staleTime이 지나면 재요청 (default: 0)
+    // refetchOnMount: true, // 컴포넌트가 마운트 될 때마다 재요청 (default: true)
+    // refetchOnWindowFocus: true, // 윈도우가 포커스 될 때마다 재요청 (default: false)
+    // refetchInterval: 10000, // 10초마다 재요청 (default: false)
+    // refetchIntervalInBackground: true, // 백그라운드에서도 재요청 (default: false)
+    enabled: false,
   });
 
   return (
     <View style={{ flex: 1, backgroundColor: "silver" }}>
       <ScrollView style={{ flex: 1 }}>
-        {data?.map((user) => (
+        {data?.data.map((user) => (
           <Text key={user.id}>{user.repo.name}</Text>
         ))}
       </ScrollView>
